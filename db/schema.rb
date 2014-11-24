fbc468da5ed0547d53f52885eaf23f6aab2b1431
Carl Consing <cmconsing@gmail.com>
2014-11-23 20:30:38 -0800

    added brand profile template

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141122215332) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "username",               default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -64,5 +65,6 @@ ActiveRecord::Schema.define(version: 20141122215332) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
