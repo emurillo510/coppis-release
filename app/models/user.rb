7567fb69c7828cb5f1250c10f62a36cb9dd114e5
Elton Murillo <emurillo510@gmail.com>
2014-11-24 22:30:34 -0800

    Added devise login via username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  ###############
+  ##validations##
+  ###############
+  validates_uniqueness_of :username
+
+
 end
