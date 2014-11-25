6510bd57380a5329c676d2dbbe5705d5f50f6371
Elton Murillo <emurillo510@gmail.com>
2014-11-25 00:30:38 -0800

    Added oauth support needs cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,45 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable #:validatable
   
   ###############
   ##validations##
   ###############
+  #validates_presence_of :username
   validates_uniqueness_of :username
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid).permit!).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      #user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      #user.oauth_expires_at = Time.at(auth.credentials.oauth_expires_at)
+      user.save!
+    end
+  end
+  
+  def self.new_with_session(params, session)
+  	if session["devise.user_attributes"]
+  		new(session["devise.user_attributes"], without_protection: true) do |user|
+  			user.attributes = params
+  		end
+  	else
+  		super
+  	end	
+  end
 
+  def password_required?
+     super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+  	if encrypted_password.blank?
+  		update_attributes(params, *options)
+  	else
+  		super
+  	end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, '29ZcHusgTHYf8tSUvdj70KVn0' , 'dCKk0FJHq8HKXxSwIdDPadFAwRmbuEd489gUWlEFvUiIOCel4K', :scope => 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
