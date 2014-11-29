9fd7ac78a8495c194d599f13d1279e4de041ca0f
Elton Murillo <emurillo510@gmail.com>
2014-11-29 00:45:14 -0800

    Added back user association with post

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,22 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	
-
+  
 	def twitter
-       user = User.from_omniauth(request.env["omniauth.auth"])
+     user = User.from_omniauth(request.env["omniauth.auth"])
 
-       if user.persisted?
-       	    flash.notice = "Signed in!"
-            sign_in user , :event => :authentication
-            redirect_to edit_user_registration_path
-
-       else
-       	    session["devise.user_attributes"] = user.attributes
-       		  redirect_to new_user_registration_url
-       end
+     if user.persisted?
+     	    flash.notice = "Signed in!"
+          sign_in user , :event => :authentication
+          redirect_to edit_user_registration_path
+     else
+     	    session["devise.user_attributes"] = user.attributes
+     		  redirect_to new_user_registration_url
+     end
 	end
 
-   private
-
+  private
   def omniauth_params
     params.require(:auth).permit(:provider,:uid, :oauth_token, :oauth_verifier)
   end
