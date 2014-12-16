0aee9d2656a6b09b5cfd442059d36175da66c325
Elton Murillo <emurillo510@gmail.com>
2014-12-16 01:04:48 -0800

    Test 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,18 +19,4 @@ class ApplicationController < ActionController::Base
     edit_user_registration_path
   end
 
-  # custom 404
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from ActiveRecord::RecordNotFound,
-                ActionController::RoutingError,
-                ActionController::UnknownController,
-                ActionController::UnknownAction,
-                ActionController::MethodNotAllowed do |exception|
-
-      # Put loggers here, if desired.
-
-      redirect_to root_path
-    end
-  end
-
 end
