231a49c9811fb26a5d2b92c5addf276772f4c085
Elton Murillo <emurillo510@gmail.com>
2014-12-18 19:06:48 -0800

    Added unique constraint back in

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.oauth_token = auth.credentials.token
       user.twitter_avatar = auth.info.image
+      user.save
     end
   end
 
