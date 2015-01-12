b1712535f464d9f833e5082d0bdbe3d2cbda2d17
Menandro <mmadlansacay@gmail.com>
2015-01-12 00:59:09 -0800

    Added jquery turbolinks gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require masonry/jquery.masonry
+//= require jquery.turbolinks
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
