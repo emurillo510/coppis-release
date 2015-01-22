cc0c8ff1556b07b74539613e56d7dccdad70d306
Elton Murillo <emurillo510@gmail.com>
2015-01-21 23:34:46 -0800

    Fixed full_name and title migration

--- db/migrate/20141125045223_add_username_to_users.rb
+++ db/migrate/20141125045223_add_username_to_users.rb
@@ -1,7 +1,7 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
-         add_column :users, :username, :string
-	 add_column :users, :full_name, :string
-	 add_column :users, :title, :string
+    add_column :users, :username, :string
+#	 add_column :users, :full_name, :string
+#	 add_column :users, :title, :string
   end
 end
--- db/migrate/20141230053959_add_is_member_to_users.rb
+++ db/migrate/20141230053959_add_is_member_to_users.rb
@@ -1,5 +1,7 @@
 class AddIsMemberToUsers < ActiveRecord::Migration
   def change
     add_column :users, :is_member, :boolean, :default => false
+	 add_column :users, :full_name, :string
+	 add_column :users, :title, :string
   end
 end
