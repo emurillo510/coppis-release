fd3c9a3c031580b4807db037cfc0ef35a2d99fae
Elton Murillo <emurillo510@gmail.com>
2015-01-21 20:43:51 -0800

    Added full name to cards

--- db/migrate/20141125045223_add_username_to_users.rb
+++ db/migrate/20141125045223_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :username, :string
+         add_column :users, :username, :string
 	 add_column :users, :full_name, :string
 	 add_column :users, :title, :string
   end
