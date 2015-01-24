45681f19e6690ec6fd3b2a13c7dd62357d3b7ec1
Elton Murillo <emurillo510@gmail.com>
2015-01-24 09:07:18 -0800

    Editted migrate 20150122073927

--- db/migrate/20150122073927_add_full_name_and_title_to_users.rb
+++ db/migrate/20150122073927_add_full_name_and_title_to_users.rb
@@ -1,5 +1,7 @@
 class AddFullNameAndTitleToUsers < ActiveRecord::Migration
   def change
+	 remove_column :users, :full_name
+	 remove_column :users, :title
     add_column :users, :full_name, :string
     add_column :users, :title, :string
   end
