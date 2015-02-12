7d53a894f24eae7c9c544228af6a3ad67757e45d
Elton Murillo <emurillo510@gmail.com>
2015-02-11 23:34:34 -0800

    Add email configuration for initializer/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,20 +66,16 @@ Rails.application.configure do
   # General Settings (Added from tutorial)
   config.app_domain = 'somedomain.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication: :plain,
-    domain: 'somedomain.com'
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  #user_name:            'coppis.co',
+  user_name:            'coppis.co',
+  password:             '180Montgomery',
+  #password:             '180Montgomery',
+  authentication:       'plain',
+  enable_starttls_auto: true  
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
