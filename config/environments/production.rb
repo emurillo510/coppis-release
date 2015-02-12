83dbf954de357af4323867c77d42091d51271c94
Elton Murillo <emurillo510@gmail.com>
2015-02-11 23:30:00 -0800

    Add email configuration for initializer/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,12 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # General Settings (Added from tutorial)
+  config.app_domain = 'somedomain.com'
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
